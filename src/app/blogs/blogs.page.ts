b3ba20ee010383aeb4ba4f1f6c7e2016a80cb974
Shepherd Siduli <shepherdsiduli@gmail.com>
2020-08-04 11:51:23 +0200

    Initial commit

--- src/app/blogs/blogs.page.ts
+++ src/app/blogs/blogs.page.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  templateUrl: 'blogs.page.html',
+  styleUrls: ['blogs.page.scss']
 })
 export class Tab1Page {
 
   constructor() {}
+
+  sayHello(){
+    console.log("Hello, World");
+  }
 
 }
